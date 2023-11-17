a42747f4985008cc9798948f376e89ecd4d203ea
Bolanle Adisa <bolanleadisa6718@gmail.com>
2023-11-17 10:38:35 -0600

    Initial Commit

---
+++ BumpBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BumpBuddy
+//
+//  Created by Bolanle Adisa on 11/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
