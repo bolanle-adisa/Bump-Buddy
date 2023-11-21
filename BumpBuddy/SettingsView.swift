209568a50d84c03893a569619845db2b23d2f01b
Bolanle Adisa <bolanleadisa6718@gmail.com>
2023-11-21 15:19:11 -0600

    settings-view

--- BumpBuddy/SettingsView.swift
+++ BumpBuddy/SettingsView.swift
@@ -1,23 +1,49 @@
-//
-//  SettingsView.swift
-//  BumpBuddy
-//
-//  Created by Bolanle Adisa on 11/21/23.
-//
-
 // SettingsView.swift
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var enableNotifications: Bool = true
+    @State private var dailyTips: Bool = true
+    @State private var weeklySummary: Bool = false
+    @State private var darkMode: Bool = false
+    @State private var language: String = "English"
+    @State private var unitOfMeasure: String = "Metric"
+
     var body: some View {
         List {
-            // Add your settings options here
-            Text("Option 1")
-            Text("Option 2")
-            Text("Option 3")
-            // You can add more options or even use more complex views
+            Section(header: Text("Personal Preferences")) {
+                Toggle(isOn: $enableNotifications) {
+                    Text("Enable Notifications")
+                }
+                Toggle(isOn: $dailyTips) {
+                    Text("Daily Health Tips")
+                }
+                Toggle(isOn: $weeklySummary) {
+                    Text("Weekly Summary Emails")
+                }
+                Picker("Language", selection: $language) {
+                    Text("English").tag("English")
+                    Text("Spanish").tag("Spanish")
+                    // Add more languages as needed
+                }
+                Picker("Unit of Measure", selection: $unitOfMeasure) {
+                    Text("Metric").tag("Metric")
+                    Text("Imperial").tag("Imperial")
+                    // Add more units as needed
+                }
+            }
+
+            Section(header: Text("App Settings")) {
+                Toggle(isOn: $darkMode) {
+                    Text("Dark Mode")
+                }
+                NavigationLink(destination: PrivacyView()) {
+                    Text("Privacy & Security")
+                }
+            }
         }
         .navigationBarTitle("Settings", displayMode: .inline)
+        .listStyle(GroupedListStyle())
     }
 }
 
