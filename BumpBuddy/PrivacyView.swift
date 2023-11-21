27209672ea81d75eb42bc86862f8592cc452e1f9
Bolanle Adisa <bolanleadisa6718@gmail.com>
2023-11-21 15:02:09 -0600

    privacy-view

--- BumpBuddy/PrivacyView.swift
+++ BumpBuddy/PrivacyView.swift
@@ -8,15 +8,51 @@
 import SwiftUI
 
 struct PrivacyView: View {
+    @State private var profileVisibility: Bool = true
+    @State private var dataSharing: Bool = false
+    @State private var twoFactorAuth: Bool = false
+    @State private var locationTracking: Bool = false
+    @State private var pushNotifications: Bool = true
+    @State private var marketingPreferences: Bool = false
+    @State private var healthDataVisibility: Bool = true
+
     var body: some View {
         List {
-            // Add your settings options here
-            Text("Option 1")
-            Text("Option 2")
-            Text("Option 3")
-            // You can add more options or even use more complex views
+            Section(header: Text("Profile Settings")) {
+                Toggle(isOn: $profileVisibility) {
+                    Text("Profile Visibility")
+                }
+                Toggle(isOn: $healthDataVisibility) {
+                    Text("Health Data Visibility")
+                }
+            }
+            
+            Section(header: Text("Security Settings")) {
+                Toggle(isOn: $twoFactorAuth) {
+                    Text("Two-Factor Authentication")
+                }
+            }
+            
+            Section(header: Text("Data Preferences")) {
+                Toggle(isOn: $dataSharing) {
+                    Text("Data Sharing with Partners")
+                }
+                Toggle(isOn: $marketingPreferences) {
+                    Text("Marketing Preferences")
+                }
+            }
+
+            Section(header: Text("App Settings")) {
+                Toggle(isOn: $locationTracking) {
+                    Text("Location Tracking")
+                }
+                Toggle(isOn: $pushNotifications) {
+                    Text("Push Notifications")
+                }
+            }
         }
-        .navigationBarTitle("Privacy", displayMode: .inline)
+        .navigationBarTitle("Privacy & Security", displayMode: .inline)
+        .listStyle(GroupedListStyle())
     }
 }
 
